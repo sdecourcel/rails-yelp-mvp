6681ca3e0b30a5feedb892d8d53e7910e12bdd75
Sibylle de Courcel <sibylle44@gmail.com>
2017-02-15 17:50:06 +0100

    render avec params ca tue

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,8 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
+  validates :rating, numericality: { only_integer: true}
+  validates :rating, inclusion: { in: (0..5), message: "must be between 0 and 5"}
   validates :rating, presence: true
-  validates :rating, numericality: { only_integer: true }
-  validates :rating, inclusion: { in: (0..5).to_a}
   validates :restaurant, presence: true
 end
